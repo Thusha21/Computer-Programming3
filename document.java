64a99c2b326319ec4f29ea4c5966bd0a7532c0ff
123 <123>
2020-09-21 10:23:54 +0100

    committed document.java

---
+++ document.java
@@ -0,0 +1,24 @@
+public class document
+{
+   private String name;
+   private String extension;
+   private String content;
+   public static void main(String args[])
+   { 
+     public void setName(){
+     }
+      public void setContent(){
+      }
+      public void setExtension(){
+    }
+     public String getName(){
+     }
+     public String getContent(){
+     }
+      public String getExtension(){
+     }
+   }
+}
+
+
+
